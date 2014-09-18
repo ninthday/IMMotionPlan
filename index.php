b20b4e19ac5326483a3c7f0ed0c5ea5272c5088b
ninthday <jeffy@ninthday.info>
2014-09-18 19:48:27 +0800

    Add bootstrap file.

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>IM Motion Planer</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
